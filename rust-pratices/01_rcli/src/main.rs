205b04c9131300a91c5f854a4f9d9d24c64f3cad
youguanxinqing <youguanxinqing@qq.com>
2024-05-02 17:10:49 +0800

    support varity type

--- rust-pratices/01_rcli/src/main.rs
+++ rust-pratices/01_rcli/src/main.rs
@@ -87,20 +87,23 @@ fn main() -> anyhow::Result<()> {
                 .from_reader(f_handler);
             let header = rdr.headers()?.clone();
 
-            let mut out = fs::File::create(format!("{}.{}", output, output_format.clone()))?;
+            let mut ret = Vec::with_capacity(10);
             for line in rdr.records() {
                 let line = line?;
                 let line = header
                     .iter()
                     .zip(line.iter())
                     .collect::<serde_json::Value>();
-                let line = match output_format {
-                    OutputFormat::Json => serde_json::to_string(&line)?,
-                    OutputFormat::Yaml => serde_yaml::to_string(&line)?,
-                };
-                out.write(line.as_bytes())?;
-                out.write(b"\n")?;
+                ret.push(line);
             }
+
+            let lines = match output_format {
+                OutputFormat::Json => serde_json::to_string(&ret)?,
+                OutputFormat::Yaml => serde_yaml::to_string(&ret)?,
+            };
+
+            let mut out = fs::File::create(format!("{}.{}", output, output_format.clone()))?;
+            out.write(lines.as_bytes())?;
         }
     };
 
