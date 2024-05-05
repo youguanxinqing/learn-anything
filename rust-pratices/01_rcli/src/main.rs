8d450655fd73988ff3a163956a0b030a2f0a4738
youguanxinqing <youguanxinqing@qq.com>
2024-05-05 11:11:05 +0800

    reformat dir structure

--- rust-pratices/01_rcli/src/main.rs
+++ rust-pratices/01_rcli/src/main.rs
@@ -1,8 +1,9 @@
-use core::fmt;
-use std::{fs, io::Write, path, str::FromStr};
+use std::path;
 
 use clap::{Parser, Subcommand};
-use csv::ReaderBuilder;
+
+mod process;
+use process::process_csv::{self, *};
 
 #[derive(Debug, Parser)]
 struct Args {
@@ -27,39 +28,6 @@ enum Command {
     },
 }
 
-#[derive(Debug, Clone, Copy)]
-enum OutputFormat {
-    Json,
-    Yaml,
-}
-
-impl fmt::Display for OutputFormat {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", Into::<&str>::into(*self))
-    }
-}
-
-impl From<OutputFormat> for &str {
-    fn from(value: OutputFormat) -> Self {
-        match value {
-            OutputFormat::Json => "json",
-            OutputFormat::Yaml => "yaml",
-        }
-    }
-}
-
-impl FromStr for OutputFormat {
-    type Err = anyhow::Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.to_lowercase().as_str() {
-            "json" => anyhow::Ok(OutputFormat::Json),
-            "yaml" => anyhow::Ok(OutputFormat::Yaml),
-            other => anyhow::bail!("Not support {}", other),
-        }
-    }
-}
-
 fn validate_file_existed(file: &str) -> anyhow::Result<String> {
     if !path::Path::new(file).exists() {
         anyhow::bail!(format!("Not found {}", file))
@@ -74,38 +42,20 @@ fn validate_output_format(format: &str) -> anyhow::Result<OutputFormat> {
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
-    match args.command {
+    
+    let result = match args.command {
         Command::Csv {
             file,
             delimiter,
             output,
             output_format,
         } => {
-            let f_handler = fs::File::open(file)?;
-            let mut rdr = ReaderBuilder::new()
-                .delimiter(delimiter as u8)
-                .from_reader(f_handler);
-            let header = rdr.headers()?.clone();
-
-            let mut ret = Vec::with_capacity(10);
-            for line in rdr.records() {
-                let line = line?;
-                let line = header
-                    .iter()
-                    .zip(line.iter())
-                    .collect::<serde_json::Value>();
-                ret.push(line);
-            }
-
-            let lines = match output_format {
-                OutputFormat::Json => serde_json::to_string(&ret)?,
-                OutputFormat::Yaml => serde_yaml::to_string(&ret)?,
-            };
-
-            let mut out = fs::File::create(format!("{}.{}", output, output_format.clone()))?;
-            out.write(lines.as_bytes())?;
+            process_csv::process_ckv(&file, delimiter, &output, output_format)
         }
     };
+    if result.is_err() {
+        eprintln!("process err: {}", result.err().unwrap())
+    }
 
     Ok(())
 }
