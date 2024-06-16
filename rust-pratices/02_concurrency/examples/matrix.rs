6efb58a3e2376aa735af1280f61f4ad7d6d9eeff
youguanxinqing <youguanxinqing@qq.com>
2024-06-17 01:00:08 +0800

    concurrency

--- rust-pratices/02_concurrency/examples/matrix.rs
+++ rust-pratices/02_concurrency/examples/matrix.rs
@@ -1,15 +1,22 @@
-use std::{fmt::{self, Debug, Display}, ops::{Add, AddAssign, Deref, Mul}, usize};
-use anyhow::{self, anyhow};
+use anyhow::anyhow;
+use std::{
+    fmt::{self, Debug, Display}, ops::{Add, AddAssign, Deref, Mul}, sync::mpsc, thread, usize
+};
+
+const MAX_PROCESS: usize = 4;
 
 #[derive(Debug)]
-struct Matrix<T> where T: Debug + Copy + Mul<Output = T> + Add + AddAssign {
+struct Matrix<T>
+where
+    T: Debug + Copy + Mul<Output = T> + Add + AddAssign,
+{
     data: Vec<T>,
     row: usize,
     col: usize,
 }
 
 pub struct Vector<T> {
-    data: Vec<T>
+    data: Vec<T>,
 }
 
 impl<T> Vector<T> {
@@ -25,10 +32,40 @@ impl<T> Deref for Vector<T> {
     }
 }
 
+pub struct MsgInput<T> {
+    idx: usize,
+    row: Vector<T>,
+    col: Vector<T>,
+}
+
+impl<T> MsgInput<T> {
+    fn new(idx: usize, row: Vector<T>, col: Vector<T>) -> Self {
+        MsgInput { idx, row, col }
+    }
+}
+
+pub struct MsgOutput<T> {
+    idx: usize,
+    value: T,
+}
+
+pub struct Msg<T> {
+    input: MsgInput<T>,
+    sender: oneshot::Sender<MsgOutput<T>>,
+}
+
+impl<T> Msg<T> {
+    fn new(input: MsgInput<T>, sender: oneshot::Sender<MsgOutput<T>>) -> Self {
+        Msg { input, sender }
+    }
+}
+
 fn dot_product<T>(a: Vector<T>, b: Vector<T>) -> anyhow::Result<T>
-where T: Debug + Default + Copy + Mul<Output = T> + Add + AddAssign {
+where
+    T: Debug + Default + Copy + Mul<Output = T> + Add + AddAssign,
+{
     if a.len() != b.len() {
-        return anyhow!("Dot product err: a.len != b.len");
+        anyhow::bail!("{}", "Dot product err: a.len != b.len");
     }
 
     let mut sum = T::default();
@@ -38,9 +75,12 @@ where T: Debug + Default + Copy + Mul<Output = T> + Add + AddAssign {
     Ok(sum)
 }
 
-impl<T> Matrix<T> where T: Debug + Copy + Mul<Output = T> + Add + AddAssign {
+impl<T> Matrix<T>
+where
+    T: Debug + Copy + Mul<Output = T> + Add + AddAssign,
+{
     fn new(data: impl Into<Vec<T>>, row: usize, col: usize) -> Self {
-        Matrix{
+        Matrix {
             data: data.into(),
             row,
             col,
@@ -48,7 +88,10 @@ impl<T> Matrix<T> where T: Debug + Copy + Mul<Output = T> + Add + AddAssign {
     }
 }
 
-impl<T> fmt::Display for Matrix<T> where T: Debug + Copy + Mul<Output = T> + Add + AddAssign + Display {
+impl<T> fmt::Display for Matrix<T>
+where
+    T: Debug + Copy + Mul<Output = T> + Add + AddAssign + Display,
+{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut chunks = Vec::new();
         for i in 0..self.row {
@@ -66,18 +109,58 @@ impl<T> fmt::Display for Matrix<T> where T: Debug + Copy + Mul<Output = T> + Add
     }
 }
 
-fn multify<T>(a: &Matrix<T>, b: &Matrix<T>) -> Matrix<T>
-where T : Debug + Copy + Mul<Output = T> + Add + AddAssign {
-    let mut c = Vec::with_capacity(a.row * b.col);
+fn multify<T>(a: &Matrix<T>, b: &Matrix<T>) -> anyhow::Result<Matrix<T>>
+where
+    T: Debug + Copy + Mul<Output = T> + Add + AddAssign + Default + Send + 'static,
+{
+    if a.col != b.row {
+        return Err(anyhow!("Matrix multiply error: a.col != b.row"));
+    }
+
+    let senders: Vec<mpsc::Sender<Msg<_>>> = (0..MAX_PROCESS).map(|_| -> mpsc::Sender<_> {
+        let (tx, rx) = mpsc::channel::<Msg<_>>();
+        thread::spawn(move || {
+            for msg in rx {
+                let value = dot_product(msg.input.row, msg.input.col).unwrap();
+                if let Err(e) = msg.sender.send(MsgOutput { idx: msg.input.idx, value }) {
+                    eprintln!("send output err: {}", e);
+                }
+            }
+        });
+        tx
+    }).collect();
+
+    let mut receivers = vec![];
+    let mut data = vec![T::default(); a.row * b.col];
     for i in 0..a.row {
         for j in 0..b.col {
-            for k in 0..a.col {
-                c[i * b.col + j] += a.data[i * a.col + k] * b.data[k * b.col + j];
+            // c[i * b.col + j] += a.data[i * a.col + k] * b.data[k * b.col + j];
+            let row = Vector::new(&a.data[i * a.col..(i + 1) * a.col]);
+            let col_data = b.data[j..]
+                .iter()
+
+                .copied()
+                .collect::<Vec<_>>();
+            let col = Vector::new(col_data);
+
+            let idx = i * b.col + j;
+            let input = MsgInput::new(idx, row, col);
+            let (tx, rx) = oneshot::channel();
+            let msg = Msg::new(input, tx);
+            if let Err(e) = senders[idx % MAX_PROCESS].send(msg) {
+                eprintln!("send input err: {}", e);
+            } else {
+                receivers.push(rx);
             }
         }
     }
-    
-    Matrix::new(c, a.row, b.col)
+
+    for rx in receivers {
+        let output = rx.recv()?;
+        data[output.idx] = output.value;
+    }
+
+    Ok(Matrix::new(data, a.row, b.col))
 }
 
 fn main() {
