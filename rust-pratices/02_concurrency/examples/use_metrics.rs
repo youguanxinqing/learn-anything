52d52f368f9a1d5a3ca1b78729ed3f9eebb84fba
youguanxinqing <youguanxinqing@qq.com>
2024-06-22 10:28:19 +0800

    优化unwrap

--- rust-pratices/02_concurrency/examples/use_metrics.rs
+++ rust-pratices/02_concurrency/examples/use_metrics.rs
@@ -1,6 +1,6 @@
 use std::{thread, time::Duration};
 
-use anyhow;
+use anyhow::{self, Result};
 use concurrency::Metrics;
 use rand::{thread_rng, Rng};
 
@@ -8,13 +8,13 @@ const N: i32 = 2;
 const M: i32 = 4;
 
 fn main() -> anyhow::Result<()> {
-    let mut metrics = Metrics::new();
+    let metrics = Metrics::new();
 
     for idx in 0..N {
-        task_worker(idx, metrics.clone());
+        let _ = task_worker(idx, metrics.clone())?;
     }
-    for idx in 0..M {
-        request_worker(metrics.clone());
+    for _ in 0..M {
+        let _ = request_worker(metrics.clone())?;
     }
 
     loop {
@@ -22,26 +22,38 @@ fn main() -> anyhow::Result<()> {
         println!("{:?}", metrics.snapshot());
     }
 
+    #[allow(unreachable_code)]
     Ok(())
 }
 
-fn task_worker(idx: i32, mut metrics: Metrics) {
-    thread::spawn(move || loop {
+fn task_worker(idx: i32, mut metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+                  loop {
         // sleep
         let mut rng = thread_rng();
         let hold_time: u64 = rng.gen_range(0..5);
         thread::sleep(Duration::from_secs(hold_time));
 
-        metrics.inc(format!("/api/login/v1/{}", idx)).unwrap();
+        metrics.inc(format!("/api/login/v1/{}", idx))?;
+                  }
+
+        #[allow(unreachable_code)]
+        Ok::<(), anyhow::Error>(())
+
     });
+    Ok(())
 }
 
-fn request_worker(mut metrics: Metrics) {
+fn request_worker(mut metrics: Metrics) -> Result<()> {
     thread::spawn(move || {
         let mut rng = thread_rng();
         let hold_time: u64 = rng.gen_range(0..3);
         thread::sleep(Duration::from_secs(hold_time));
 
-        metrics.inc("/api/request/v1").unwrap();
+        metrics.inc("/api/request/v1")?;
+
+        #[allow(unreachable_code)]
+        Ok::<(), anyhow::Error>(())
     });
+    Ok(())
 }
