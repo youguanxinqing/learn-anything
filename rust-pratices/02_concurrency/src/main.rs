24d4e300d9c39de86d5f81e41019d2e82bfb8cdc
youguanxinqing <youguanxinqing@qq.com>
2024-06-28 22:54:16 +0800

    async block

--- rust-pratices/02_concurrency/src/main.rs
+++ rust-pratices/02_concurrency/src/main.rs
@@ -1,1 +1,16 @@
-fn main() {}
+use std::io;
+
+use async_std::{fs::read_to_string, task::block_on};
+
+#[async_std::main]
+async fn main() -> Result<(), io::Error> {
+    let f = async {
+        let s = read_to_string("Cargo.toml").await?;
+        println!("{}", s);
+        Ok::<(), io::Error>(())
+    };
+
+    block_on(f)?;
+
+    Ok(())
+}
