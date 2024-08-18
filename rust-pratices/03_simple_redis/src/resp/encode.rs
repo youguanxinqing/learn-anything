02bcd3e3a69911af596bea5219195f1f4ab1befe
youguanxinqing <youguanxinqing@qq.com>
2024-08-18 12:32:09 +0800

    complete RespFrame encode api

--- rust-pratices/03_simple_redis/src/resp/encode.rs
+++ rust-pratices/03_simple_redis/src/resp/encode.rs
@@ -1,6 +1,28 @@
+use core::f64;
+use std::{collections::{HashMap, HashSet}, usize};
+
 use bytes::{BufMut, BytesMut};
 
-use super::{NullArray, NullBulkString, RespEncode, SimpleError, SimpleString, RespFrame};
+use super::{Map, NullArray, NullBulkString, RespEncode, RespFrame, RespNull, Set, SimpleError, SimpleString};
+
+impl RespEncode for RespFrame {
+    fn encode(self) -> Vec<u8> {
+        match self {
+            RespFrame::SimpleString(data) => data.encode(),
+            RespFrame::Error(data) => data.encode(),
+            RespFrame::Integer(data) => data.encode(),
+            RespFrame::BulkString(data) => data.encode(),
+            RespFrame::NullBulkString(data) => data.encode(),
+            RespFrame::Array(data) => data.encode(),
+            RespFrame::NullArray(data) => data.encode(),
+            RespFrame::Null(data) => data.encode(),
+            RespFrame::Boolean(data) => data.encode(),
+            RespFrame::Double(data) => data.encode(),
+            RespFrame::Map(data) => data.encode(),
+            RespFrame::Set(data) => data.encode(),
+        }
+    }
+}
 
 
 // Simple String: +OK\r\n
@@ -45,24 +67,20 @@ impl RespEncode for NullBulkString {
     }
 }
 
+const BUF_LEN: usize = 4096;
+
 // Arrays: *<number-of-elements>\r\n<element-1>...<element-n>
-// impl RespEncode for Vec<RespFrame> {
-//     fn encode(self) -> Vec<u8> {
-//         let mut array = BytesMut::new();
-//         array.put_slice(b"*");
-//         array.put_slice(self.len().to_string().as_bytes());
-//         array.put_slice(b"\r\n");
-//         array.put_slice(self.iter().map(|item| {
-//             match item {
-//                 RespFrame::SimpleString(data) => data.encode().as_ref(),
-//                 RespFrame::Error(data) => data.encode().as_ref(),
-//                 RespFrame::Integer(data) => data.encode().as_ref(),
-//             }
-//         }));
-//
-//         array.into()
-//     }
-// }
+impl RespEncode for Vec<RespFrame> {
+    fn encode(self) -> Vec<u8> {
+        let mut buf = Vec::with_capacity(BUF_LEN);
+        buf.extend_from_slice(b"*");
+        buf.extend_from_slice(self.len().to_string().as_bytes());
+        for frame in self {
+            buf.extend_from_slice(frame.encode().as_ref());
+        }
+        buf
+    }
+}
 
 // Null arrays: *-1\r\n
 impl RespEncode for NullArray {
@@ -71,9 +89,67 @@ impl RespEncode for NullArray {
     }
 }
 
+// Null: _\r\n
+impl RespEncode for RespNull {
+    fn encode(self) -> Vec<u8> {
+        "_\r\n".into()
+    }
+}
+
+// Boolean: #<t|f>\r\n
+impl RespEncode for bool {
+    fn encode(self) -> Vec<u8> {
+        format!("#{}\r\n", if self {"t"} else {"f"}).into()
+    }
+}
+
+// Double: ,[<+|->]<integral>[.<fractional>][<E|e>[sign]<exponent>]\r\n
+impl RespEncode for f64 {
+    fn encode(self) -> Vec<u8> {
+        let value = match self {
+            x if x > f64::NEG_INFINITY && x < f64::INFINITY => format!("{}", self),
+            x if x >= f64::INFINITY => "inf".to_string(),
+            x if x <= f64::NEG_INFINITY => "-inf".to_string(),
+            _ => "nan".into(),
+        };
+        format!(",{}\r\n", value).into()
+    }
+}
+
+// Map: %<number-of-entries>\r\n<key-1><value-1>...<key-n><value-n>
+impl RespEncode for Map {
+    fn encode(self) -> Vec<u8> {
+        let mut buf = Vec::with_capacity(BUF_LEN);
+        buf.extend_from_slice(b"%");
+        buf.extend_from_slice(self.len().to_string().as_bytes());
+        buf.extend_from_slice(b"\r\n");
+        for (k, v) in self.0 {
+            buf.extend_from_slice(SimpleString::new(k).encode().as_ref());
+            buf.extend_from_slice(v.encode().as_ref());
+        }
+
+        buf
+    }
+}
+
+// Set: ~<number-of-elements>\r\n<element-1>...<element-n>
+impl RespEncode for Set {
+    fn encode(self) -> Vec<u8> {
+        let mut buf = Vec::with_capacity(BUF_LEN);
+        buf.extend_from_slice(b"~");
+        buf.extend_from_slice(self.len().to_string().as_bytes());
+        buf.extend_from_slice(b"\r\n");
+        for frame in self.0 {
+            buf.extend_from_slice(frame.encode().as_ref());
+        }
+        
+        buf
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::resp::RespEncode;
+    use crate::resp::{Set, Map, RespEncode, RespFrame};
 
     #[test]
     fn test_vec_u8_encoding() {
@@ -84,4 +160,33 @@ mod tests {
         assert_eq!("$5\r\nhello\r\n".to_string().as_bytes(), stream);
     }
 
+    #[test]
+    fn test_double_encoding() {
+        let v: f64 = std::f64::NAN;
+        let stream = v.encode();
+        println!("encode result: {:?}", String::from_utf8_lossy(&stream));
+        
+        assert_eq!(",nan\r\n".to_string().as_bytes(), stream);
+        assert_eq!(",1.23\r\n".to_string().as_bytes(), 1.23.encode());
+        assert_eq!(",inf\r\n".to_string().as_bytes(), f64::INFINITY.encode());
+        assert_eq!(",-inf\r\n".to_string().as_bytes(), f64::NEG_INFINITY.encode());
+    }
+
+    #[test]
+    fn test_map_encoding() {
+        let mut m = Map::new();
+        m.insert("one_key".into(), RespFrame::SimpleString("Hello".into()));
+        m.insert("one_value".into(), RespFrame::SimpleString("World".into()));
+
+        println!("{:?}", String::from_utf8_lossy(&m.encode()));
+    }
+
+    #[test]
+    fn test_set_encoding() {
+        let mut m = Set::new();
+        m.insert(RespFrame::SimpleString("See".into()));
+        m.insert(RespFrame::SimpleString("You".into()));
+
+        println!("{:?}", String::from_utf8_lossy(&m.encode()));
+    }
 }
--- rust-pratices/03_simple_redis/src/resp/mod.rs
+++ rust-pratices/03_simple_redis/src/resp/mod.rs
@@ -2,7 +2,7 @@ pub mod decode;
 pub mod encode;
 
 
-use std::collections::{HashMap, HashSet};
+use std::{collections::{HashMap, HashSet}, ops::{Deref, DerefMut}};
 use bytes::{BytesMut};
 
 trait RespEncode {
@@ -19,6 +19,7 @@ impl RespDecode for BytesMut {
     }
 }
 
+#[derive(Eq, Hash, PartialEq)]
 pub enum RespFrame {
     SimpleString(SimpleString),
     Error(SimpleError),
@@ -29,14 +30,121 @@ pub enum RespFrame {
     NullArray(NullArray),
     Null(RespNull),
     Boolean(bool),
-    Double(f64),
-    Map(HashMap<String, RespFrame>),
-    Set(HashSet<RespFrame>),
+    Double(Double),
+    Map(Map),
+    Set(Set),
 }
 
+#[derive(Eq, Hash, PartialEq)]
 pub struct SimpleString(String);
+
+impl SimpleString {
+    pub fn new(v: impl Into<String>) -> Self {
+        Self(v.into())
+    }
+}
+
+impl From<&str> for SimpleString {
+    fn from(value: &str) -> Self {
+        SimpleString::new(value)
+    }
+}
+
+#[derive(Eq, Hash, PartialEq)]
 pub struct SimpleError(String);
+
+#[derive(Eq, Hash, PartialEq)]
 pub struct RespNull;
+
+#[derive(Eq, Hash, PartialEq)]
 pub struct NullArray;
+
+#[derive(Eq, Hash, PartialEq)]
 pub struct NullBulkString;
 
+pub struct Double(f64);
+
+impl Deref for Double {
+    type Target = f64;
+    
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl std::cmp::PartialEq for Double {
+    fn eq(&self, other: &Self) -> bool {
+        self == other
+    }
+}
+
+impl std::cmp::Eq for Double {
+    fn assert_receiver_is_total_eq(&self) {
+        todo!()
+    }
+}
+
+impl std::hash::Hash for Double {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        todo!()
+    }
+}
+
+#[derive(Eq, PartialEq)]
+pub struct Map(HashMap<String, RespFrame>);
+
+impl Map {
+    pub fn new() -> Map {
+        Self(HashMap::new())
+    }
+}
+
+impl Deref for Map {
+    type Target = HashMap<String, RespFrame>;
+    
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Map {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl std::hash::Hash for Map {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        todo!()
+    }
+}
+
+#[derive(Eq, PartialEq)]
+pub struct Set(HashSet<RespFrame>);
+
+impl Set {
+    fn new() -> Set {
+        Set(HashSet::new())
+    }
+}
+
+impl Deref for Set {
+    type Target = HashSet<RespFrame>;
+    
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Set {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+
+impl std::hash::Hash for Set {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        todo!()
+    }
+}
