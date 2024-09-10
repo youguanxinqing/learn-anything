84a5f9f82c6e0d5f55d74a5070c00a0631f9af96
youguanxinqing <youguanxinqing@qq.com>
2024-09-10 22:53:16 +0800

    impl Decode for specified type

--- rust-pratices/03_simple_redis/src/resp/decode.rs
+++ rust-pratices/03_simple_redis/src/resp/decode.rs
@@ -1,5 +1,10 @@
-use super::{RespDecode, RespError, RespFrame, SimpleString};
-use bytes::{Buf, BytesMut};
+use std::{fmt::format, str::FromStr};
+
+use super::{
+    Double, NullArray, NullBulkString, RespDecode, RespError, RespFrame, RespNull, SimpleError,
+    SimpleString,
+};
+use bytes::{Buf, BufMut, BytesMut};
 
 impl RespDecode for RespFrame {
     fn decode(buf: BytesMut) -> Result<Self, RespError> {
@@ -13,27 +18,41 @@ impl RespDecode for RespFrame {
     }
 }
 
+fn lookup_pos_before_end(buf: &BytesMut) -> usize {
+    let mut end = 0;
+    for i in (1..buf.len()).rev() {
+        if buf[i] == b'\n' && buf[i - 1] == b'\r' {
+            end = i - 1;
+            break;
+        }
+    }
+    return end;
+}
+
+fn validate_len_of_buf(buf: &BytesMut) -> Result<(), RespError> {
+    if buf.len() < 3 {
+        return Err(RespError::NotComplete);
+    }
+    Ok(())
+}
+
+fn validate_starts_with(buf: &BytesMut, prefix: &[u8], error_msg: &str) -> Result<(), RespError> {
+    if !buf.starts_with(prefix) {
+        return Err(RespError::InvalidFrameType(format!(
+            "{}, but got: {:?}",
+            error_msg, buf
+        )));
+    }
+    Ok(())
+}
+
 impl RespDecode for SimpleString {
     fn decode(buf: BytesMut) -> Result<Self, RespError> {
-        if buf.len() < 3 {
-            return Err(RespError::NotComplete);
-        }
+        validate_len_of_buf(&buf)?;
+        validate_starts_with(&buf, b"+", "expect: SimpleString(+)")?;
 
-        if !buf.starts_with(b"+") {
-            return Err(RespError::InvalidFrameType(format!(
-                "expect: SimpleString(+), got: {:?}",
-                buf
-            )));
-        }
-
-        // search \r\n
-        let mut end = 0;
-        for i in (1..buf.len()).rev() {
-            if buf[i] == b'\n' && buf[i - 1] == b'\r' {
-                end = i - 1;
-                break;
-            }
-        }
+        // search position before \r\n
+        let end = lookup_pos_before_end(&buf);
         if end == 0 {
             return Err(RespError::NotComplete);
         }
@@ -42,6 +61,88 @@ impl RespDecode for SimpleString {
         match s {
             Err(e) => Err(RespError::InvalidFrame(e.to_string())),
             Ok(s) => Ok(SimpleString(s)),
+        }
+    }
+}
+
+impl RespDecode for SimpleError {
+    fn decode(buf: BytesMut) -> Result<Self, RespError> {
+        validate_len_of_buf(&buf)?;
+        validate_starts_with(&buf, b"-", "expect: SimpleError(-)")?;
+
+        // search position before \r\n
+        let end = lookup_pos_before_end(&buf);
+        if end == 0 {
+            return Err(RespError::NotComplete);
+        }
+
+        let s = String::from_utf8(buf[1..end].to_vec());
+        match s {
+            Err(e) => Err(RespError::InvalidFrame(e.to_string())),
+            Ok(s) => Ok(SimpleError(s)),
+        }
+    }
+}
+
+impl RespDecode for RespNull {
+    fn decode(buf: BytesMut) -> Result<Self, RespError> {
+        validate_len_of_buf(&buf)?;
+
+        if buf != "_\r\n" {
+            return Err(RespError::InvalidFrameType(format!(
+                "expect: RespNull(_\r\n), got: {:?}",
+                buf
+            )));
+        }
+
+        Ok(RespNull)
+    }
+}
+
+impl RespDecode for NullArray {
+    fn decode(buf: BytesMut) -> Result<Self, RespError> {
+        validate_len_of_buf(&buf)?;
+
+        if buf != "*-1\r\n" {
+            return Err(RespError::InvalidFrameType(format!(
+                "expect: NullArray(*-1\r\n), got: {:?}",
+                buf
+            )));
+        }
+
+        Ok(NullArray)
+    }
+}
+
+impl RespDecode for NullBulkString {
+    fn decode(buf: BytesMut) -> Result<Self, RespError> {
+        validate_len_of_buf(&buf)?;
+
+        if buf != "$-1\r\n" {
+            return Err(RespError::InvalidFrameType(format!(
+                "expect: NullBulkString($-1\r\n), got: {:?}",
+                buf
+            )));
+        }
+
+        Ok(NullBulkString)
+    }
+}
+
+impl RespDecode for Double {
+    fn decode(buf: BytesMut) -> Result<Self, RespError> {
+        validate_len_of_buf(&buf)?;
+        validate_starts_with(&buf, b",", "expect: Double(,)")?;
+
+        let end = lookup_pos_before_end(&buf);
+        if end == 0 {
+            return Err(RespError::NotComplete);
+        }
+
+        let s: &str = &String::from_utf8_lossy(&buf[1..end]);
+        match FromStr::from_str(s) {
+            Err(_) => Ok(Double(f64::NAN)),
+            Ok(value) => Ok(Double(value)),
         }
     }
 }
